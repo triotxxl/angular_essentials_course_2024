11649004326e1aa82014cbdee0d67b0d212c0334
David Hellner <David.Hellner@scheer-pas.com>
2025-03-27 14:34:10 +0100

    start project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,37 @@ import { Component } from '@angular/core';
   standalone: true,
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  dummyTrafficData = [
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ];
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
+  currentStatus = 'online';
+}

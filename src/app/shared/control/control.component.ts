e97c9950364ac1c551572e5b23a48e84861aa606
David Hellner <David.Hellner@scheer-pas.com>
2025-05-14 18:55:43 +0200

    dynamic styles / classes

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,8 +15,20 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
-  host: { class: 'control' },
+  host: { class: 'control', '(click)': 'onClick()' },
 })
 export class ControlComponent {
   @Input({ required: true }) label!: string;
+  // @HostBinding('class') className = 'control';
+
+  // @HostListener('click') onClick() {
+  //   console.log('click');
+  // }
+
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('click');
+    console.log(this.el);
+  }
 }

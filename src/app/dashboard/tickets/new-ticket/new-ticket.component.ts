32d147ba0ce8198c4accca482ce1ce8a44d02020
David Hellner <David.Hellner@scheer-pas.com>
2025-05-19 17:01:34 +0200

    reset form with viewchild

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,8 +11,9 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
   onSubmit(title: string, text: string, form: HTMLFormElement) {
     console.log('Title: ', title, 'Text: ', text);
-    form.reset();
+    this.form?.nativeElement.reset();
   }
 }

2e214b35ec9cf7b9e5bc3fc95c45ca0656bdeaa3
David Hellner <David.Hellner@scheer-pas.com>
2025-08-19 14:44:55 +0200

    add value binding with ngModel

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '0';
+  enteredDuration = '0';
 
+  onSubmit() {
+    console.log('Form submitted with values:');
+    console.log('Initial Investment:', this.enteredInitialInvestment);
+    console.log('Annual Investment:', this.enteredAnnualInvestment);
+    console.log('Expected Return:', this.enteredExpectedReturn);
+    console.log('Duration:', this.enteredDuration);
+
+    this.enteredAnnualInvestment = '0';
+    this.enteredExpectedReturn = '0';
+    this.enteredDuration = '0';
+    this.enteredInitialInvestment = '0';
+  }
 }

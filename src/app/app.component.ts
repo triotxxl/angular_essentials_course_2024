a105fb399748ec46b2083c4b012d880c743231f4
David Hellner <David.Hellner@scheer-pas.com>
2025-08-28 13:06:07 +0200

    add result table
    - add model.ts
    - add investment-result component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import type { InvestmentInput } from '../investment.input.model';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
-  calculateInvestmentResults(data: {
-    initialInvestment: number;
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
     annualInvestment: number;
-    expectedReturn: number;
-    duration: number;
-  }) {
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[] = [];
+
+  calculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, annualInvestment, expectedReturn, duration } =
       data;
     const annualData = [];
@@ -35,6 +41,7 @@ export class AppComponent {
         totalAmountInvested: initialInvestment + annualInvestment * year,
       });
     }
-    console.log(annualData);
+
+    this.resultsData = annualData;
   }
 }
